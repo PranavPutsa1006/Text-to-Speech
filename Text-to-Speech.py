b75445d75466b044e1c2cb659ed071d7f6d7b469
Rama Krishna Pranav Putsa <pranavputsa@gmail.com>
2020-08-20 16:48:56 +0530

    Create Text-to-Speech.py

---
+++ Text-to-Speech.py
@@ -0,0 +1,11 @@
+def voice_speaks(output):
+    c_hr, c_minu, c_m = tell_time()
+    c_t = str(c_hr) + ":" + str(c_minu) + " " + str(c_m)
+    print("Gideon: ", output, "       ("+c_t+")")
+    toSpeak = pyttsx3.init()
+    voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
+    toSpeak.setProperty('voice', voice_id)
+    toSpeak.setProperty('rate', 165)
+    toSpeak.setProperty('volume', 0.8)
+    toSpeak.say(output)
+    toSpeak.runAndWait()
